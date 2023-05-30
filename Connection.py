72ef8b5cbaee527ae77c0ce589c3b00f6e265d0c
Laura Espina <lespina@gmail.com>
2023-05-31 01:03:00 +0200

    Uploaded files for v1.0

---
+++ Connection.py
@@ -0,0 +1,12 @@
+from sqlalchemy.orm import declarative_base
+from sqlalchemy.engine import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+from personal_info_v1 import sql_dialect_driver, username, password, host, database_name
+
+def CreateConnection():
+    db_url = f'{sql_dialect_driver}://{username}:{password}@{host}/{database_name}'
+    engine = create_engine(db_url)
+    Session = sessionmaker(engine)
+    session = Session()
+    Base = declarative_base()
+    return engine, session, Base
