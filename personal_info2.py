41fce69d2546c614cbd5a6a5f5a3f2c05a05b414
Laura Espina <lespina@gmail.com>
2023-05-30 15:14:51 +0200

    Removed files to re-tag

--- personal_info2.py
+++
@@ -1,9 +0,0 @@
-import os 
-
-work_folder = str(os.path.abspath(os.getcwd()).replace(os.sep, '/')+'/')
-sql_dialect_driver = 'mysql'
-username = 'root'
-password = ''
-host = 'host'
-database_name = 'card_db'
-email = ''
